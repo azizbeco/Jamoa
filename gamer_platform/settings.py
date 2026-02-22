30752d0538a238bcff017bb92fc58658763d1b4f
Tohirjonov <azizbeco@kali.azizbek>
2026-02-22 11:52:39 -0500

    Update production settings (ALLOWED_HOSTS, Whitenoise, STATIC_ROOT)

--- gamer_platform/settings.py
+++ gamer_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-jrp03fltkfeh!-d219759lu_jk(*t58u!nl6uwa)hix-50$0ia
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # Xavfsizlik uchun keyinchalik o'z domeningizni yozasiz
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,4 +135,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'login'
+
+# Static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
