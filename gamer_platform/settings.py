e39d051e6e707360e30bb7c80c15124ad9153b60
Tohirjonov <azizbeco@kali.azizbek>
2026-02-22 12:06:24 -0500

    Configure environment variables for SECRET_KEY and DEBUG

--- gamer_platform/settings.py
+++ gamer_platform/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jrp03fltkfeh!-d219759lu_jk(*t58u!nl6uwa)hix-50$0ia'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-jrp03fltkfeh!-d219759lu_jk(*t58u!nl6uwa)hix-50$0ia')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']  # Xavfsizlik uchun keyinchalik o'z domeningizni yozasiz
 
