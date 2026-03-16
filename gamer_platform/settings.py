664dd11bbd2a65cf3c850d9bb48710b89d8def45
Juratbek Bekmurodov <juratbek0105@gmail.com>
2026-03-16 14:37:54 +0500

    Prepare for Alwaysdata deployment

--- gamer_platform/settings.py
+++ gamer_platform/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jrp03fltkfeh!-d219759lu_jk(*t58u!nl6uwa)hix-50$0ia'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-jrp03fltkfeh!-d219759lu_jk(*t58u!nl6uwa)hix-50$0ia')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -122,7 +123,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
 
-import os
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
