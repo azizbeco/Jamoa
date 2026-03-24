cc332b5d32f559ac7e08d5547631d190617d6992
Juratbek Bekmurodov <juratbek0105@gmail.com>
2026-03-24 17:43:34 +0500

    fix(ui): Rebuild production assets to include Midnight RGB styling

--- frontend/.output/server/chunks/build/styles.mjs
+++ frontend/.output/server/chunks/build/styles.mjs
@@ -2,16 +2,16 @@ const interopDefault = r => r.default || r || [];
 const styles = {
   "pages/teams/[id].vue": () => import('./_id_-styles.CJt6Lh8G.mjs').then(interopDefault),
   "pages/teams/[id].vue?vue&type=style&index=0&scoped=a62ab70c&lang.css": () => import('./_id_-styles.CJt6Lh8G.mjs').then(interopDefault),
-  "../node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.DuDrf-v0.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-500.vue": () => import('./error-500-styles.8IYEHzz6.mjs').then(interopDefault),
+  "../node_modules/nuxt/dist/app/components/error-404.vue": () => import('./error-404-styles.DuDrf-v0.mjs').then(interopDefault),
   "components/PostInteraction.vue": () => import('./PostInteraction-styles.BE0h70SF.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=204d37bf&lang.css": () => import('./error-404-styles.DuDrf-v0.mjs').then(interopDefault),
   "../node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=d349100d&lang.css": () => import('./error-500-styles.8IYEHzz6.mjs').then(interopDefault),
   "components/PostInteraction.vue?vue&type=style&index=0&scoped=a316ca99&lang.css": () => import('./PostInteraction-styles.BE0h70SF.mjs').then(interopDefault),
-  "components/TeamCard.vue": () => import('./TeamCard-styles.uzTupSG3.mjs').then(interopDefault),
-  "components/TeamCard.vue?vue&type=style&index=0&scoped=af25f202&lang.css": () => import('./TeamCard-styles.uzTupSG3.mjs').then(interopDefault),
   "components/AuthForm.vue": () => import('./AuthForm-styles.RH7gts10.mjs').then(interopDefault),
-  "components/AuthForm.vue?vue&type=style&index=0&scoped=863ba118&lang.css": () => import('./AuthForm-styles.RH7gts10.mjs').then(interopDefault)
+  "components/TeamCard.vue": () => import('./TeamCard-styles.uzTupSG3.mjs').then(interopDefault),
+  "components/AuthForm.vue?vue&type=style&index=0&scoped=863ba118&lang.css": () => import('./AuthForm-styles.RH7gts10.mjs').then(interopDefault),
+  "components/TeamCard.vue?vue&type=style&index=0&scoped=af25f202&lang.css": () => import('./TeamCard-styles.uzTupSG3.mjs').then(interopDefault)
 };
 
 export { styles as default };
