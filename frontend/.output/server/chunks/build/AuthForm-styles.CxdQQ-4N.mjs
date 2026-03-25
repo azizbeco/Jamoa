28794c408ac4bd7fa6e2f522ce8c9242586f6b04
Juratbek Bekmurodov <juratbek0105@gmail.com>
2026-03-25 09:29:54 +0500

    chore: stop tracking build output

--- frontend/.output/server/chunks/build/AuthForm-styles.CxdQQ-4N.mjs
+++
@@ -1,8 +0,0 @@
-const AuthForm_vue_vue_type_style_index_0_scoped_a72029e5_lang = ".animate-shake[data-v-a72029e5]{animation:shake-a72029e5 .4s cubic-bezier(.36,.07,.19,.97) both}@keyframes shake-a72029e5{10%,90%{transform:translate3d(-1px,0,0)}20%,80%{transform:translate3d(2px,0,0)}30%,50%,70%{transform:translate3d(-4px,0,0)}40%,60%{transform:translate3d(4px,0,0)}}";
-
-const AuthFormStyles_CxdQQ4N = [
-  AuthForm_vue_vue_type_style_index_0_scoped_a72029e5_lang
-];
-
-export { AuthFormStyles_CxdQQ4N as default };
-//# sourceMappingURL=AuthForm-styles.CxdQQ-4N.mjs.map
--- frontend/.output/server/chunks/build/PostInteraction-styles.CEnOxlFV.mjs
+++
@@ -1,8 +0,0 @@
-const PostInteraction_vue_vue_type_style_index_0_scoped_4b1d31bc_lang = ".custom-scrollbar[data-v-4b1d31bc]::-webkit-scrollbar{width:4px}.custom-scrollbar[data-v-4b1d31bc]::-webkit-scrollbar-track{background:#0000004d}.custom-scrollbar[data-v-4b1d31bc]::-webkit-scrollbar-thumb{background:#dc26264d;border-radius:10px}.custom-scrollbar[data-v-4b1d31bc]::-webkit-scrollbar-thumb:hover{background:#dc262699}";
-
-const PostInteractionStyles_CEnOxlFV = [
-  PostInteraction_vue_vue_type_style_index_0_scoped_4b1d31bc_lang
-];
-
-export { PostInteractionStyles_CEnOxlFV as default };
-//# sourceMappingURL=PostInteraction-styles.CEnOxlFV.mjs.map
